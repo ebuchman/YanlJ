77c08b31782aa792974c422f4f7af67f5dabab65
Ethan Buchman <ebuchman@uoguelph.ca>
2014-02-08 17:04:21 -0500

    cleaner code, ajax, delete entries

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php session_start(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+<title>YanlJ, a non-linear JanlY.</title>
+</head>
+
+<body>
+
+<?php include("wiki.php"); ?>
+
+<h1>YanlJ, a non-linear JanlY</h1>
+
+<?php check_login(); check_logout(); present_login() ?>
+
+<?php 
+    if ($_SESSION['LOGGED_IN']){
+        edit_delete();
+        load_recent_posts();
+        load_new_entry_form();
+    }
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  method="post" style="position:fixed; top:20px; right:100px;"><p>	
+<input type="submit" name="exit" value="Logout"></form>
+
+</body>
+</html>
