d8a1108d1a374ef034affeba16f3c6c0a3bd984b
Ethan Buchman <ebuchman@uoguelph.ca>
2014-02-11 12:58:18 -0500

    safer, edit entries working, mathjax

--- index.php
+++ index.php
@@ -1,27 +1,111 @@
-<?php session_start(); ?>
+<?php session_start(); 
+if (!isset($_SESSION['LOGGED_IN'])) $_SESSION['LOGGED_IN'] = 0;?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html>
 <head>
 <title>YanlJ, a non-linear JanlY.</title>
+<script type="text/x-mathjax-config">
+/*
+MathJax.Hub.Config({
+      tex2jax: {
+              inlineMath: [['$','$'], ['\\(','\\)']],
+                      processEscapes: true
+                        }
+      });
+ */
+</script>
+<script type="text/javascript"
+src="https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
+
+<script type="text/javascript">
+
+// when an entry name is clicked on, a content box should appear with the information 
+function get_entry_data(name){
+    if (window.XMLHttpRequest)
+        xmlhttp=new XMLHttpRequest();
+    else
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.onreadystatechange=function(){
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            var content = xmlhttp.responseText;
+            document.getElementById('entry_content_box').innerHTML = content;
+            //MathJax.Hub.Queue(["Typeset",MathJax.Hub,"entry_content_box"]); 
+            MathJax.Hub.Typeset('entry_content_box');
+
+            // prevent escaping backslashes (for mathjax) when content gets sent to edit_entry_data
+            //  but this is a bug if the text itself has a backslash - makes it uneditable!!!
+            content = content.replace(/\\/g, '\\\\');
+
+            var eddel = '<form id="delete_entry" style="display:inline-block;" action="" method="post" > \
+            <a href="javascript:;" onClick="document.getElementById(\'delete_entry\').submit();">delete</a>  \
+            <input type="hidden" name="entry_to_delete" value="' + name + '" > \
+        </form>  &nbsp&nbsp \
+        <a href="#" id="edit_link" onClick="edit_entry_data(\'' + name + '\', \'' + content + '\');">edit</a></div> ';
+            
+           document.getElementById('edit_delete_links').innerHTML = eddel;   
+           document.getElementById('content_header').innerHTML = "<h4>"+name+"</h4>";
+        }
+    }
+    xmlhttp.open("POST", "load_content.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("name="+name);
+    return false;
+}
+
+function edit_entry_data(name, content){
+        document.getElementById('new_entry_name_box').value = name;
+        document.getElementById('new_entry_content_box').value = content;
+}
+
+// when the edit link is clicked, the content box should become a textbox, but getting this to work is a nightmare (consensus: use jQuery...)
+function edit_entry_data_(name){
+        box = document.getElementById('entry_content_box'); //.innerHTML = "fuck";
+//        var edit_box_form = document.createElement('form');
+  //      edit_box_form.action="
+        var edit_box = document.createElement('textarea');
+        edit_box.style.position = "fixed";
+        edit_box.style.top = "400px";
+        edit_box.style.left = "300px";
+        edit_box.id = 'edit_box';
+        edit_box.value = box.innerHTML;
+        document.body.appendChild(edit_box);
+        edit_box.rows = 5; edit_box.columns=50;
+        var edit_link  = document.getElementById('edit_link');
+        //edit_box.onchange=push_entry_changes();
+        /*var done_form= "<form id=\"update_entry\" action=\"\" method=\"post\"> \ 
+            <a href=\"#\" onClick=\"document.getElementById('update_entry').submit();\">done</a>"; \ 
+            <input type=\"hidden\" name=\"new_content\" value=\" 
+         */
+        var done_link = "<a href\"#\" onClick=\"push_entry_changes()\">done</a>";
+        edit_link.innerHTML = done_link;
+}
+</script>
+
 </head>
 
 <body>
-<?php if ($_SERVER['HTTPS'] !== 'on') {
-    die("Must be a secure connection."); }?>
-
 <?php include("wiki.php"); ?>
-
+<?php https(); ?>
 <h1>YanlJ, a non-linear JanlY</h1>
-
 <?php check_login(); check_logout(); present_login() ?>
 
 <?php 
-    if ($_SESSION['LOGGED_IN']){
+    if (htmlspecialchars($_SESSION['LOGGED_IN'])){
         edit_delete();
         load_recent_posts();
         load_new_entry_form();
     }
 ?>
+
+<div id="edit_delete_links" style="position:fixed; top:500px; left:300px;"></div>
+<div id="big_content_box" style="position:fixed; top:400px; left:300px;">
+<div id="content_header"></div>
+<div id="entry_content_box"></div>
+</div>
+
+<!--<input type="input" id="fucker">-->
 
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  method="post" style="position:fixed; top:20px; right:100px;"><p>	
 <input type="submit" name="exit" value="Logout"></form>
--- wiki.php
+++ wiki.php
@@ -1,34 +1,8 @@
-<script type="text/javascript">
-function get_entry_data(name){
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-            document.getElementById('entry_content_box').innerHTML = xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("POST", "load_content.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("name="+name);
-    return false;
-}
-</script>
-
-<div id="entry_content_box" style="position:fixed; top:400px; left:300px;">
-
-
-</div>
-
-
 <?php
-
 function load_recent_posts(){
-    if ($_SESSION['LOGGED_IN']){
-        $usr = $_SESSION['USR_NAME'];
-        $pwd = $_SESSION['PASSWORD'];
+    if (htmlspecialchars($_SESSION['LOGGED_IN'])){
+        $usr = pg_escape_string($_SESSION['USR_NAME']);
+        $pwd = pg_escape_string($_SESSION['PASSWORD']);
         $db_name = 'wikidb'; 
 
         $con = pg_connect("host=localhost dbname=wikidb user=$usr password=$pwd");
@@ -50,77 +24,72 @@ function load_recent_posts(){
     }
 }
 
-/*
-                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  method="post">	
-                <input type="hidden" name="entry" value="<?php echo $row[0]; ?>"><p>
-                <input type="submit" name="<?php echo $row[0]; ?>" value="<?php echo $row[0]; ?>"></form> <?php 
- */
-
-
 
 function load_new_entry_form(){?>
 
         <div>
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  method="post" style="position:fixed; top:100px; left:300px;"><p>
         <h4>Add a new entry below:</h4>
-        Entry Name: <input type="text" name="entry_name"><p>
-        Entry Content: <textarea  name="entry_content" rows=5 cols=50></textarea><p>
+        Entry Name: <input id="new_entry_name_box" type="text" name="entry_name"><p>
+        Entry Content: <textarea  id="new_entry_content_box" name="entry_content" rows=5 cols=50></textarea><p>
         <input type="submit" name="new_entry" value="Add Entry"></form>
         </div>
 <?php }
 
 
 function add_new_entry($con){
-    if ($_POST['new_entry']){
+    if (isset($_POST['new_entry'])){
+
         if ($con)
         { 
             $name = htmlspecialchars($_POST['entry_name']);
             $content = htmlspecialchars($_POST['entry_content']);
-            $sql = "INSERT INTO Entries VALUES ('$name', '$content')";
-            $result = pg_query($con, $sql);
+            if (pg_query($con, "SELECT * FROM Entries WHERE entryname = '$name'")){
+                /* couldnt get the UPDATE working...
+                echo $content;
+                $sql = "UPDATE Entries SET entry='$content' WHERE entryname='$name'";
+                $result = pg_query($con, $sql);
+                echo pg_fetch_row($result);
+                 */
+                $sql="DELETE FROM Entries WHERE entryname= '$name'";
+                $result = pg_query($con, $sql);
+            }
+            //else{
+
+                $sql = "INSERT INTO Entries VALUES ('$name', '$content')";
+                $result = pg_query($con, $sql);
+            //}
         }	
     }
 }
 
-function delete_entry($entry_name){
-            echo "<form id=\"delete_entry\" action=\"\" method=\"post\" >
-               <a href=\"javascript:;\" onClick=\"document.getElementById('delete_entry').submit();\">delete</a>
-               <input type=\"hidden\" name=\"entry_to_delete\" value=\"".$entry_name."\">
-                </form>";
-}
 
 function edit_delete(){
-    if ($_POST['entry_to_delete']){
-        if ($_SESSION['LOGGED_IN']){
-            $usr = $_SESSION['USR_NAME'];
-            $pwd = $_SESSION['PASSWORD'];
+    if (isset($_POST['entry_to_delete'])){
+        if (htmlspecialchars($_SESSION['LOGGED_IN'])){
+            $usr = pg_escape_string($_SESSION['USR_NAME']);
+            $pwd = pg_escape_string($_SESSION['PASSWORD']);
             $db_name = 'wikidb'; 
+
+            $entry_to_delete = pg_escape_string($_POST['entry_to_delete']);
 
             $con = pg_connect("host=localhost dbname=wikidb user=$usr password=$pwd");
             if ($con)
             { 
-                $sql = "DELETE FROM Entries WHERE entryname='".$_POST['entry_to_delete']."';";
+                $sql = "DELETE FROM Entries WHERE entryname='". $entry_to_delete ."';";
                 $result = pg_query($con, $sql);
 
                 pg_free_result($result);
                 pg_close($con);
             }
         }
-
-
-
     }
 }
-
-
-?>
-
-<?php 
 
 // authorization functions
 
 function check_login(){
-    if ($_POST['login']){
+    if (isset($_POST['login'])){
       $usr = pg_escape_string($_POST['usr']);
       $pwd = pg_escape_string($_POST['pwd']);
       // is this a bad idea?
@@ -128,15 +97,18 @@ function check_login(){
       $_SESSION['PASSWORD'] = $pwd;
 
       $db_name = 'wikidb'; 
-      $con = pg_connect("host=localhost dbname=wikidb user=$usr password=$pwd");
-
+      // use the @ to suppress php/psql error message
+      $con =@pg_connect("host=localhost dbname=wikidb user=$usr password=$pwd");
+     
       if ($con)
        $_SESSION['LOGGED_IN'] = 1;
+      else echo "Incorrect Credentials.";
     }
 }
 function check_logout(){
-    if ($_POST['exit']){ 	
+    if (isset($_POST['exit'])){ 	
        $_SESSION['LOGGED_IN'] = 0;
+       $_SESSION['current_entry'] = 0;
        session_destroy();
     }
     if (!isset($_SESSION['USR_NAME']))
@@ -148,9 +120,10 @@ function check_logout(){
 
 function present_login(){
     if(!$_SESSION['LOGGED_IN']) : ?>
-        <h3>Please log in to use the wiki</h3>
+        <h3>Welcome <?php echo htmlspecialchars($_SERVER['REMOTE_ADDR']) ?>. Please log in to use the wiki</h3>
 
-        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <?php // Note: $_SERVER['PHP_SELF'] comes from the url and is an attack vector.  Must be escaped.  Or, just use $_SERVER['SCRIPT_NAME'] ?>
+        <form action="<?php htmlspecialchars($_SERVER['SCRIPT_NAME']); ?>" method="post">
           Username: <input type="text" name="usr">
           <p>
           Password: <input type="password" name="pwd">
@@ -158,11 +131,14 @@ function present_login(){
           <input type="submit" name="login" value="Login">
         </form> 
     <?php else :
-        echo "<h3 style='position:fixed; top:50px; left:100; padding:10px;'>Hello, " .  $_SESSION['USR_NAME'] . ".  Enjoy using YanlJ</h3>" ;
+        echo "<h3 style='position:fixed; top:50px; left:100; padding:10px;'>Hello, " .  htmlspecialchars($_SESSION['USR_NAME']) . ".  Enjoy using YanlJ</h3>" ;
     endif; 
 }
+
+function https(){ 
+ if (!isset($_SERVER['HTTPS'])) {
+    echo "<h2>YanlJ is only accessible over a secure connection.  Try <a href=https://".htmlspecialchars($_SERVER['HTTP_HOST']).">here</a></h2>" ;die(); }
+     
+}
+
 ?>
-
-
-
-
