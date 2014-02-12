590f3349e402169b10bbb7da83fc54fb42ca9b6d
Ethan Buchman <ebuchman@uoguelph.ca>
2014-02-11 19:53:17 -0500

    security fixes.  escapings

--- load_content.php
+++ load_content.php
@@ -1,24 +1,25 @@
 <?php session_start();
 
 function load_entry_content($entry_name){
-    $_GLOBAL['current_entry'] = $entry_name;
+    $escaped_entry_name = htmlspecialchars(pg_escape_string($entry_name));
+    $_GLOBAL['current_entry'] = $escaped_entry_name;
 
     if ($_SESSION['LOGGED_IN']){
-        $usr = $_SESSION['USR_NAME'];
-        $pwd = $_SESSION['PASSWORD'];
+        $usr = htmlspecialchars(pg_escape_string($_SESSION['USR_NAME']));
+        $pwd = htmlspecialchars(pg_escape_string($_SESSION['PASSWORD']));
         $db_name = 'wikidb'; 
 
         $con = pg_connect("host=localhost dbname=wikidb user=$usr password=$pwd");
         if ($con)
         { 
-            $sql = "SELECT * FROM Entries WHERE entryname = '$entry_name'";
+            $sql = "SELECT * FROM Entries WHERE entryname = '$escaped_entry_name'";
             $result = pg_query($con, $sql);
             while($row = pg_fetch_array($result))
-                echo $row[1];
+                echo htmlspecialchars($row[1]);
             pg_free_result($result);
             pg_close($con);
         }
     }
 }
-load_entry_content($_POST['name']);
+load_entry_content(htmlspecialchars($_POST['name']));
 ?>
