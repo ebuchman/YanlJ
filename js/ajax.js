5061c0b6ebeb4f9b8cad5ab619518ca8d5e295b3
Ethan Buchman <ethan@coinculture.info>
2014-06-20 05:26:52 +0000

    general ajax framework. major ajax.js cleanup

--- js/ajax.js
+++ js/ajax.js
@@ -1,59 +1,62 @@
 
+// general framework for ajax calls. _callback functions are in callback.js
+
+function new_request_obj(){
+    if (window.XMLHttpRequest)
+        return new XMLHttpRequest();
+    else
+        return new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+function request_callback(xmlhttp, _func, args){
+    xmlhttp.onreadystatechange=function(){
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+		args.unshift(xmlhttp);
+		_func.apply(this, args);
+        }
+    }
+}
+
+function make_request(xmlhttp, method, path, async, params){
+    xmlhttp.open(method, path, async);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var s = "", i = 0;
+    for (var k in params){
+	if (i > 0)
+  	  s += "&";
+	s += k+"="+encodeURIComponent(params[k]);
+	i++;
+    }
+    //xmlhttp.setRequestHeader("Content-length", s.length); // important?
+    xmlhttp.send(s);
+}
+
+
 // inplace is either nothing or is the element corresponding to the bubble to change
 function get_entry_data(name, inplace){
     inplace = typeof inplace !== 'undefined' ? inplace : 0
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-		_get_entry_data(xmlhttp, name, inplace);
-        }
-    }
-    xmlhttp.open("POST", "ajax/load_content.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("name="+encodeURIComponent(name));
+
+    xmlhttp = new_request_obj();
+    request_callback(xmlhttp, _get_entry_data, array(name, inplace));
+    make_request(xmlhttp, "POST", "ajax/load_content.php", true, {"name":name});
     return false;
 }
 
 function edit_entry_data(name, content, element){
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-		_edit_entry_data(xmlhttp, name, content, element);
-        }
-    }
-    xmlhttp.open("POST", "ajax/load_content.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("name="+encodeURIComponent(name));
+    xmlhttp = new_request_obj();
+    request_callback(xmlhttp, _edit_entry_data, array(name, content, element));
+    make_request(xmlhttp, "POST", "ajax/load_content.php", true, {"name":name});
     return false;
 }
 
 // called when clicking `done` after editing
 function replace_entry_data(name, content, element){
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-
     var new_title = element.getElementsByClassName('edit_title')[0].value;
     var new_content = element.getElementsByClassName('edit_entry_content')[0].value;
 
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-		_replace_entry_data(xmlhttp, name, content, element, new_title, new_content);
-        }
-    }
-    var query_string = "old_title="+encodeURIComponent(name)+"&new_title="+encodeURIComponent(new_title)+"&content="+encodeURIComponent(new_content);
-    xmlhttp.open("POST", "ajax/replace_entry.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader("Content-length", query_string.length); 
-    xmlhttp.send(query_string);
+    xmlhttp = new_request_obj();
+    request_callback(xmlhttp, _replace_entry_data, array(name, content, element, new_title, new_content));
+    make_request(xmlhttp, "POST", "ajax/replace_entry.php", true, {"old_title":name, "new_title":new_title, "content":new_content});
     return false;
 }
 
@@ -61,59 +64,34 @@ function displaySearchResults(keystrokes){
     if (keystrokes.length == 0)
     { document.getElementById('search_results').innerHTML = ""; return;}
 
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    xmlhttp = new_request_obj();
 
+    // make callback even?
     xmlhttp.onreadystatechange=function(){
         if (xmlhttp.readyState==4 && xmlhttp.status==200){
 	    document.getElementById('search_results').innerHTML = xmlhttp.responseText;
         }
     }
-    xmlhttp.open("POST", "ajax/search_db.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("keystrokes="+keystrokes);
+    make_request(xmlhttp, "POST", "ajax/search_db.php", true, {"keystrokes":keystrokes});
+    return false;
 }
 
 function add_new_entry(){
     var name = document.getElementById('new_entry_name_box').value;
     var content = document.getElementById('new_entry_content_box').value;
 
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-		_add_new_entry(xmlhttp, name, content);
-        }
-    }
-    var query_string = "old_title="+encodeURIComponent('')+"&new_title="+encodeURIComponent(name)+"&content="+encodeURIComponent(content);
-    xmlhttp.open("POST", "ajax/replace_entry.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader("Content-length", query_string.length); 
-    xmlhttp.send(query_string);
+    xmlhttp = new_request_obj();
+    request_callback(xmlhttp, _add_new_entry, array(name, content));
+    make_request(xmlhttp, "POST", "ajax/replace_entry.php", true, {"old_title":'', "new_title":name, "content":content});
     return false;
 }
-
 
 function delete_entry(name){
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-	  _delete_entry(xmlhttp, name);
-        }
-    }
-    xmlhttp.open("POST", "ajax/delete_entry.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("name="+encodeURIComponent(name));
+    xmlhttp = new_request_obj();
+    request_callback(xmlhttp, _delete_entry, array(name));
+    make_request(xmlhttp, "POST", "ajax/delete_entry.php", true, {"name":name});
     return false;
 }
-
 
 function register_new_user(login_form){
     var name = login_form.usr.value;
@@ -123,18 +101,8 @@ function register_new_user(login_form){
 
     // validate....
 
-    if (window.XMLHttpRequest)
-        xmlhttp=new XMLHttpRequest();
-    else
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    xmlhttp.onreadystatechange=function(){
-    if (xmlhttp.readyState==4 && xmlhttp.status==200){
-	  _register_new_user(xmlhttp, login_form);
-        }
-    }
-    post_data = "name="+encodeURIComponent(name)+"&pwd="+encodeURIComponent(pwd)+"&email="+encodeURIComponent(email);
-    xmlhttp.open("POST", "ajax/register_user.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send(post_data);
+    xmlhttp = new_request_obj();
+    request_callback(xmlhttp, _register_new_user, array(login_form));
+    make_request(xmlhttp, "POST", "ajax/register_user.php", true, {"name":name, "pwd"=pwd, "email"=email});
     return false;
 }
